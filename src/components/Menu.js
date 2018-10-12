727b9397afdb0a7814a89db4636304da8e49503a
Randy Maile <randy.maile@sharp.com>
2018-10-11 18:02:55 -0700

    changed menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -61,7 +61,7 @@ const Menu = () => {
           </li>
           <li>
             <Link to="/contact/" activeStyle={activeLinkStyle}>
-              Contact
+              Contact us
             </Link>
           </li> 
           <li>
